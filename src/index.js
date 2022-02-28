ded2d940df3365685a22b39029aeb21d5425d6dd
MohamedEssam1695 <toma126542@gmail.com>
2022-02-28 14:53:02 +0200

    setup NotPad-- project

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Text,View} from 'react-native';
+
+
+
+
+
+const App = () => {
+
+
+  return (
+    <View>
+     <Text>{"NotPad--"}</Text>
+    </View>
+  );
+};
+
+
+
+export default App;
