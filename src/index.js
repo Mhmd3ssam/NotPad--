29d4666a4b2dff1c7a4d0cf7e824f7dc7b6b308e
MohamedEssam1695 <toma126542@gmail.com>
2022-02-28 17:43:27 +0200

    setup navagation  dependencies

--- src/index.js
+++ src/index.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import { NativeBaseProvider } from "native-base";
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Home from './screens/Home';
+import Details from './screens/Details';
+
 import Camera from './components/Camera';
 import useCamera from './Hooks/useCamera';
 import ActionSheet from './components/ActionSheet';
+
+
+
 const App = () => {
+    const Tab = createMaterialTopTabNavigator();
+    const Stack = createNativeStackNavigator();
+
     const [takePhotoFromCamera,imageUri,takePhotoFromGallery,imageUriGallary] = useCamera();
 
-    return (<>
+    return (      
+    <NavigationContainer>
         <NativeBaseProvider>
-            <Text>{"NotPad--"}</Text>
             <ActionSheet camera={takePhotoFromCamera} gallery={takePhotoFromGallery}/>
             <Camera imageUri={imageUri} imageUriGallary={imageUriGallary}/>
         </NativeBaseProvider>
 
-    </>);
+        </NavigationContainer>);
 };
 
 
---
+++ src/screens/Notes/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+const Notes = ()=>{
+    return(
+        <View>
+            <Text>{'Notes'}</Text>
+        </View>
+    )
+}
+
+export default Notes;
