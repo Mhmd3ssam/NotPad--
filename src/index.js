bf6dc5e0e11f5d266987144075f56e1ec33799f1
MohamedEssam1695 <toma126542@gmail.com>
2022-03-04 19:20:51 +0200

    implement async stroage

--- src/index.js
+++ src/index.js
@@ -1,21 +1,33 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { PersistGate } from 'redux-persist/integration/react'
 import { NavigationContainer } from '@react-navigation/native';
 import Stack from './Navagation/Stack';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { persistStore, persistReducer } from 'redux-persist';
 
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { reducer } from './Store/Reducer';
 
-const store = createStore(reducer);
+const persistConfig = {
+    key: 'root',
+    storage: AsyncStorage,
+}
+const persistedReducer = persistReducer(persistConfig, reducer);
+let store = createStore(persistedReducer)
+let persistor = persistStore(store);
+
 
 
 const App = () => {
     return (
         <Provider store={store}>
-            <NavigationContainer>
-                <Stack />
-            </NavigationContainer>
+            <PersistGate loading={null} persistor={persistor}>
+                <NavigationContainer>
+                    <Stack />
+                </NavigationContainer>
+            </PersistGate>
         </Provider>);
 };
 
